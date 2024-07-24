aca64ce41cc77c28732e139b3a24cf7f198b2cfe
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2024-07-23 20:38:49 -0400

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import Hero from "@/app/components/hero";
+import AboutUs from "./components/aboutUs";
+import Portfolio from "./components/portfolio";
+import Testimonials from "./components/testimonials";
+import Footer from "./components/footer";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <main>
+      <Hero />
+      <AboutUs />
+      <Portfolio />
+      <Testimonials />
+      <Footer />
+    </main>
+  );
 }
